a05e4a3d8e75b66e11c108d0d12b35624017220f
Mehtab <mehtab36143101@gmail.com>
2026-07-29 15:50:53 +0500

    change more icon

--- frontend/src/components/Icons.jsx
+++ frontend/src/components/Icons.jsx
@@ -152,9 +152,9 @@ export function CalendarIcon(props) {
 export function MoreIcon(props) {
     return (
         <Icon {...props}>
-            <line x1="4" y1="6" x2="20" y2="6" />
-            <line x1="4" y1="12" x2="20" y2="12" />
-            <line x1="4" y1="18" x2="20" y2="18" />
+            <circle cx="5" cy="12" r="1" />
+            <circle cx="12" cy="12" r="1" />
+            <circle cx="19" cy="12" r="1" />
         </Icon>
     )
 }
