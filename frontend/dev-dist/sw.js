77a48667716a9828af35721d100832b8788d8fdf
Mehtab <mehtab36143101@gmail.com>
2026-07-29 06:30:36 +0500

    use env_file for backend to load all env vars from prod .env automatically

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.kdqtomgn4i8"
+    "revision": "0.j24rk2592f8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
